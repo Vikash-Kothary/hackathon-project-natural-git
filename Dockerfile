9c2d1b98d03ae84a72831a061d5bb8ddc6722f95
Vikash Kothary <kothary.vikash@gmail.com>
2018-01-13 16:33:08 +0000

    Seperating docker container with dependencies and code
    Due to internet able to download pip packages every time

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,8 @@
+# Internet doesn't work so I'm storing all the requirements in a docker container
+
 FROM python:3.4
 
-MAINTAINER Vikash Kothary "kothary.vikash@gmail.com"
-
-COPY ./app /app
+COPY ./app/requirements.txt /app/requirements.txt
 WORKDIR /app
 
 RUN pip install -r requirements.txt
-
-ENTRYPOINT ["python"]
-CMD ["main.py"]
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # run.sh
 
-docker-compose up -d
+
+docker build -t python_env .
+#docker run --rm -ti --name python_env python_env /bin/bash
+
+docker build -t natural_git app/
+docker run --rm -ti --name natural_git -p 5000:5000 natural_git
+
+#docker-compose up -d
 #curl 
 #echo $(curl -s $(docker port ngrok_tunnel 4040)/api/tunnels | grep -P "http://.*?ngrok.io" -oh)
